2baab15749d3d16069eaa241bbfa820540eca3fa
Max <74422818+Ev0lu@users.noreply.github.com>
2025-02-03 11:11:09 +0300

    Delete src directory

--- src/app/store/reducers/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from '@/app/store';
-
-export const selectUserSettings = (state: RootState) => ({
-    name: state.userSettings.name,
-    phone: state.userSettings.phone,
-    password: state.userSettings.password,
-    checkIsValidPhone: state.userSettings.checkIsValidPhone,
-    changeMode: state.userSettings.changeMode,
-});
--- src/shared/api/index.ts
+++
@@ -1,4 +0,0 @@
-export { userApi } from '@/shared/api/user';
-export { authApi, useLoginUser } from '@/shared/api/auth';
-export { useValidation } from '@/shared/api/errors';
-export { useLinkIdChecker, useQrScanner, terrariumApi } from '@/shared/api/terrarium';
