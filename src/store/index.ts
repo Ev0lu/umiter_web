e734be4c5dadd87d68778575b81e09a336e9b273
Max <74422818+Ev0lu@users.noreply.github.com>
2025-02-03 11:10:07 +0300

    Delete src/store directory

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import {createStore, combineReducers} from 'redux'
-import { profileRegistrationReducer } from './profileRegistrationReducer'
-
-const rootReducer = combineReducers({
-    profileRegistrationReducer: profileRegistrationReducer
-})
-
-export const store = createStore(rootReducer)
